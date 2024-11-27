76295cc90dc3c3870a453a71b8e33814eefbea91
abdulazizOmonov <abdulaziomonov4@gmail.com>
2024-11-27 23:36:33 +0500

    tunnel_opening_start

--- dispatcher_cli/client/http_tunnel.go
+++ dispatcher_cli/client/http_tunnel.go
@@ -1,10 +1,36 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"log"
+	urlPackage "net/url"
+	"os/user"
+
+	"github.com/gorilla/websocket"
+	"golang.org/x/net/websocket"
+)
 
 type HttpTunnel struct {
+	Host    string `bson:"host"`
+	Token   string `bson:"token"`
+	Warning string `bson:"warning"`
+	Error   string `bson:"error"`
 }
 
 func openHttpTunnel(port int, subdomain string, ctx context.Context) {
-
+	if subdomain == "" {
+		u, err := user.Current()
+		if err != nil {
+			log.Fatalf("Please specify -subdomain")
+		}
+		subdomain = u.Username
+	}
+	query := fmt.Sprintf("port=%d&username=%s&version=%s", port, subdomain, version)
+	url := urlPackage.URL{Scheme: "wss", Host: baseHost, Path: "/create_tunnel", RawQuery: query} // agar baseHost ssl/tls support qilsa wss directly ip orqali bo'layotgan bo'lsa ws
+	ws, _, err := websocket.DefaultDialer.Dial(url.String(), nil)
+	if err != nil {
+		log.Fatalf("Error connecting to %s: %s", baseHost, err.Error())
+	}
+	defer ws.Close()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module cloud-dispatcher-router
 
 go 1.23.2
+
+require (
+	github.com/gorilla/websocket v1.5.3
+	golang.org/x/net v0.31.0
+)
